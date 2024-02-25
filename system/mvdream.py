4ed94fb2d54be30330cf753ab166244960f33932
runjiali-rl <runjiali@outlook.com>
2024-02-25 14:59:26 +0000

    mvdream multi sds update

--- system/mvdream.py
+++ system/mvdream.py
@@ -7,6 +7,192 @@ from threestudio.systems.base import BaseLift3DSystem
 from threestudio.utils.misc import cleanup, get_device
 from threestudio.utils.ops import binary_cross_entropy, dot
 from threestudio.utils.typing import *
+import numpy as np
+
+
+@threestudio.register("mvdream-system-multi-bounded")
+class MVDreamMultiBoundedSystem(BaseLift3DSystem):
+    @dataclass
+    class Config(BaseLift3DSystem.Config):
+        visualize_samples: bool = False
+        bound_path: str = None
+        prompt_list: List[str] = None
+
+    cfg: Config
+
+    def configure(self) -> None:
+        # set up geometry, material, background, renderer
+        super().configure()
+        assert self.cfg.bound_path is not None, "bound_path should be provided."
+        self.bound = np.load(self.cfg.bound_path)
+        assert len(self.bound.shape) == 4, "bound should be a 4D array."
+        self.bound = torch.tensor(self.bound).to(self.device) # shape (num_volumes, height, width, 2)
+        self.guidance = threestudio.find(self.cfg.guidance_type)(self.cfg.guidance)
+        self.prompt_processor_list = []
+        for prompt in self.cfg.prompt_list:
+            self.cfg.prompt_processor.prompt=prompt
+
+            self.prompt_processor = threestudio.find(self.cfg.prompt_processor_type)(
+                self.cfg.prompt_processor
+            )
+            self.prompt_processor_list.append(self.prompt_processor)
+        self.prompt_utils_list = []
+        for prompt_processor in self.prompt_processor_list:
+            prompt_processor = prompt_processor()
+            self.prompt_utils_list.append(prompt_processor)
+
+
+    def forward(self, batch: Dict[str, Any],
+                bound: np.ndarray) -> Dict[str, Any]:
+        return self.renderer(**batch, bound=bound)
+
+    def training_step(self, batch, batch_idx):
+        
+        out_list = [self(batch, bound) for bound in self.bound[:-1]]
+
+        guidance_out_list = [self.guidance(out["comp_rgb"], self.prompt_utils_list[idx], **batch) for idx, out in enumerate(out_list)]
+
+        loss = 0.0
+        for out, guidance_out in zip(out_list, guidance_out_list):
+            for name, value in guidance_out.items():
+                self.log(f"train/{name}", value)
+                if name.startswith("loss_"):
+                    loss += value * self.C(self.cfg.loss[name.replace("loss_", "lambda_")])
+
+            if self.C(self.cfg.loss.lambda_orient) > 0:
+                if "normal" not in out:
+                    raise ValueError(
+                        "Normal is required for orientation loss, no normal is found in the output."
+                    )
+                loss_orient = (
+                    out["weights"].detach()
+                    * dot(out["normal"], out["t_dirs"]).clamp_min(0.0) ** 2
+                ).sum() / (out["opacity"] > 0).sum()
+                self.log("train/loss_orient", loss_orient)
+                loss += loss_orient * self.C(self.cfg.loss.lambda_orient)
+
+            if self.C(self.cfg.loss.lambda_sparsity) > 0:
+                loss_sparsity = (out["opacity"] ** 2 + 0.01).sqrt().mean()
+                self.log("train/loss_sparsity", loss_sparsity)
+                loss += loss_sparsity * self.C(self.cfg.loss.lambda_sparsity)
+
+            if self.C(self.cfg.loss.lambda_opaque) > 0:
+                opacity_clamped = out["opacity"].clamp(1.0e-3, 1.0 - 1.0e-3)
+                loss_opaque = binary_cross_entropy(opacity_clamped, opacity_clamped)
+                self.log("train/loss_opaque", loss_opaque)
+                loss += loss_opaque * self.C(self.cfg.loss.lambda_opaque)
+
+            # z variance loss proposed in HiFA: http://arxiv.org/abs/2305.18766
+            # helps reduce floaters and produce solid geometry
+            if self.C(self.cfg.loss.lambda_z_variance) > 0:
+                loss_z_variance = out["z_variance"][out["opacity"] > 0.5].mean()
+                self.log("train/loss_z_variance", loss_z_variance)
+                loss += loss_z_variance * self.C(self.cfg.loss.lambda_z_variance)
+
+            if (
+                hasattr(self.cfg.loss, "lambda_eikonal")
+                and self.C(self.cfg.loss.lambda_eikonal) > 0
+            ):
+                loss_eikonal = (
+                    (torch.linalg.norm(out["sdf_grad"], ord=2, dim=-1) - 1.0) ** 2
+                ).mean()
+                self.log("train/loss_eikonal", loss_eikonal)
+                loss += loss_eikonal * self.C(self.cfg.loss.lambda_eikonal)
+
+            for name, value in self.cfg.loss.items():
+                self.log(f"train_params/{name}", self.C(value))
+
+        return {"loss": loss}
+
+    def validation_step(self, batch, batch_idx):
+        out = self(batch, self.bound[-1])
+        self.save_image_grid(
+            f"it{self.true_global_step}-{batch['index'][0]}.png",
+            (
+                [
+                    {
+                        "type": "rgb",
+                        "img": out["comp_rgb"][0],
+                        "kwargs": {"data_format": "HWC"},
+                    },
+                ]
+                if "comp_rgb" in out
+                else []
+            )
+            + (
+                [
+                    {
+                        "type": "rgb",
+                        "img": out["comp_normal"][0],
+                        "kwargs": {"data_format": "HWC", "data_range": (0, 1)},
+                    }
+                ]
+                if "comp_normal" in out
+                else []
+            )
+            + [
+                {
+                    "type": "grayscale",
+                    "img": out["opacity"][0, :, :, 0],
+                    "kwargs": {"cmap": None, "data_range": (0, 1)},
+                },
+            ],
+            name="validation_step",
+            step=self.true_global_step,
+        )
+
+    def on_validation_epoch_end(self):
+        pass
+
+    def test_step(self, batch, batch_idx):
+        out = self(batch)
+        self.save_image_grid(
+            f"it{self.true_global_step}-test/{batch['index'][0]}.png",
+            (
+                [
+                    {
+                        "type": "rgb",
+                        "img": out["comp_rgb"][0],
+                        "kwargs": {"data_format": "HWC"},
+                    },
+                ]
+                if "comp_rgb" in out
+                else []
+            )
+            + (
+                [
+                    {
+                        "type": "rgb",
+                        "img": out["comp_normal"][0],
+                        "kwargs": {"data_format": "HWC", "data_range": (0, 1)},
+                    }
+                ]
+                if "comp_normal" in out
+                else []
+            )
+            + [
+                {
+                    "type": "grayscale",
+                    "img": out["opacity"][0, :, :, 0],
+                    "kwargs": {"cmap": None, "data_range": (0, 1)},
+                },
+            ],
+            name="test_step",
+            step=self.true_global_step,
+        )
+
+    def on_test_epoch_end(self):
+        self.save_img_sequence(
+            f"it{self.true_global_step}-test",
+            f"it{self.true_global_step}-test",
+            "(\d+)\.png",
+            save_format="mp4",
+            fps=30,
+            name="test",
+            step=self.true_global_step,
+        )
+
+
 
 
 
@@ -21,6 +207,7 @@ class MVDreamBoundedSystem(BaseLift3DSystem):
     def configure(self) -> None:
         # set up geometry, material, background, renderer
         super().configure()
+        
         self.guidance = threestudio.find(self.cfg.guidance_type)(self.cfg.guidance)
         self.prompt_processor = threestudio.find(self.cfg.prompt_processor_type)(
             self.cfg.prompt_processor
